a4b9703979605db01bf74ef4d02923281fbb6664
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-07 10:45:28 +0100

    add the App icons

---
+++ WeatherApp-IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp-IOS
+//
+//  Created by Amjad Oudeh on 07.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp-IOS/WeatherApp_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_IOSApp.swift
+//  WeatherApp-IOS
+//
+//  Created by Amjad Oudeh on 07.02.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
