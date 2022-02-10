051aff31aec076521adf6298fe5591f1827afad5
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-10 09:09:34 +0100

    Added the location to the content view & the location

--- WeatherApp-IOS/Views/ContentView.swift
+++ WeatherApp-IOS/Views/ContentView.swift
@@ -12,9 +12,18 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            WelcomeView()
-                .environmentObject(locationManager)
-        }.background( )
+            if let location = locationManager.location {
+                Text("Your coordinates are: \(location.longitude),\(location.latitude)")
+            } else {
+                if locationManager.isLoading {
+                    // ProgrdssView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }.background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354) )
+            .preferredColorScheme(.dark)
     }
 }
 
