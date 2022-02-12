5986425da94d6258ce558fa878dc41d46bffd68c
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-12 23:41:15 +0100

    Created a model data file!

---
+++ WeatherApp-IOS/Views/WeatherView.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherView.swift
+//  WeatherApp-IOS
+//
+//  Created by Amjad Oudeh on 10.02.22.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
+
