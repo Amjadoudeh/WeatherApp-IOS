f7290b529a45f1c40e6de06a7036242d284f90c0
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-15 22:23:16 +0100

    Created an Extension to round the Double in the Json file

---
+++ WeatherApp-IOS/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherApp-IOS
+//
+//  Created by Amjad Oudeh on 15.02.22.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
