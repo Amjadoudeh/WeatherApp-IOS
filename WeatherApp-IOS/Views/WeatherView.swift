16f30a812b081fbd4da6458b088de3a2b3d77085
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-15 22:16:56 +0100

    added a weather previewer

--- WeatherApp-IOS/Views/WeatherView.swift
+++ WeatherApp-IOS/Views/WeatherView.swift
@@ -17,7 +17,7 @@ struct WeatherView: View {
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherView()
+        WeatherView(weather: previewWeather)
     }
 }
 
