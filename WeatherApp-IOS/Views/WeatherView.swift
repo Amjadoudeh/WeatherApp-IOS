ab14cccbc83b53bcf74e04f7ac3ac0c46706a2a8
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-16 23:22:06 +0100

    adding the name of the city , time and Date

--- WeatherApp-IOS/Views/WeatherView.swift
+++ WeatherApp-IOS/Views/WeatherView.swift
@@ -14,7 +14,12 @@ struct WeatherView: View {
         ZStack(alignment: .leading){
             VStack{
                 VStack(alignment: .leading, spacing: 5){
+                    Text(weather.name)
+                        .bold()
+                        .font(.title)
                     
+                    Text("Today,\(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
