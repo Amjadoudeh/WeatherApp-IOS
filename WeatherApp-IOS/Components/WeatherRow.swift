d2d221e28565bc1924d0df6da1e83026ade03cf7
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-17 20:47:23 +0100

    created Weather Row and styled the logo

---
+++ WeatherApp-IOS/Components/WeatherRow.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherRow.swift
+//  WeatherApp-IOS
+//
+//  Created by Amjad Oudeh on 17.02.22.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var logo: String
+    var name: String
+    var value: String
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(systemName: logo)
+                .font(.title2)
+                .frame(width: 20, height: 20)
+                .padding()
+                .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.888))
+                .cornerRadius(50)
+            
+            
+        }
+    }
+}
+
+struct WeatherRow_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherRow()
+    }
+}
