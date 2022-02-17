b140e82d35c2eb40c8de5c964baa7947669c3d28
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-17 20:26:33 +0100

    added an extension for the View

--- WeatherApp-IOS/Extensions.swift
+++ WeatherApp-IOS/Extensions.swift
@@ -6,9 +6,18 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
 
 extension Double {
     func roundDouble() -> String {
         return String(format: "%.0f", self)
     }
 }
+
+extension View {
+    func cornerRaduis(_ raduis: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(raduis: raduis, corners: corners))
+    }
+}
+
