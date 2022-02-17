1d8d24b75d1cd069be621f7a87eb59d771812a89
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-17 20:30:43 +0100

    added a RoundedCorner struct

--- WeatherApp-IOS/Extensions.swift
+++ WeatherApp-IOS/Extensions.swift
@@ -17,7 +17,16 @@ extension Double {
 
 extension View {
     func cornerRaduis(_ raduis: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape(RoundedCorner(raduis: raduis, corners: corners))
+        clipShape(RoundedCorner(radius: raduis, corners: corners))
     }
 }
 
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect , byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
